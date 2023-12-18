8e70f7eea3619a23da72aa6d79223f53e3ef7931
Pablo Medialdea Torres <pablo.torres@kemmlit.de>
2023-12-18 17:42:17 +0100

    Zeigt beim Clicken eine "X" oder ein "O"

--- projects/01-tick-tack-toe/src/App.jsx
+++ projects/01-tick-tack-toe/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handelClick = () => {
+    updateBoard(index);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div onClick={handelClick} className={className} key={index}>
+      {children}
+    </div>
+  );
+};
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    console.log(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
+
+  return (
+    <main className="board">
+      <h1>Tick tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} updateBoard={updateBoard} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O} </Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
