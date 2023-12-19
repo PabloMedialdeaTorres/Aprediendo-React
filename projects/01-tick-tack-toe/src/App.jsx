3626eaefc5c39a3849bf27f16d6eef2f8f161090
Pablo Medialdea Torres <pablo.torres@kemmlit.de>
2023-12-19 11:01:41 +0100

    Win lose logic hinzugefügt

--- projects/01-tick-tack-toe/src/App.jsx
+++ projects/01-tick-tack-toe/src/App.jsx
@@ -5,6 +5,19 @@ const TURNS = {
   O: "o",
 };
 
+// TODO: optimizar para que no haya que escribir cada combinacion ganadora
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
 
@@ -22,20 +35,49 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  // null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null);
+
+  // revisar todas las combinaciones, para ver si X u O han ganado
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // si no hay, devuelve null porque no hay ganador
+    return null;
+  };
 
   const updateBoard = (index) => {
+    // si ya tiene algo no hagas nada
+    if (board[index] || winner) {
+      return;
+    }
+
+    //actualizar el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-    console.log(newBoard);
 
+    // cambia el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
     <main className="board">
-      <h1>Tick tac toe</h1>
+      <h1>Tic tac toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
